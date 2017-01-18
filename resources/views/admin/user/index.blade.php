becdf2a8d57ce579b4070023433682318b6fdeb7
Ikikay <Ikikay@Altais>
2017-01-18 10:50:23 +0100

    Commencement de l'administration des Utilisateurs dans le BackOffice

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layout_back')
+
+@section('title')
+<h1>
+    Administration des utilisateurs
+    <small>- Rechercher, Modifier et supprimer des utilisateurs</small>
+</h1>
+@stop
+
+@section('content')
+
+
+<!-- Main content -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-border">
+                {!! Form::open(['route' => "register", 'method' => 'get']) !!}
+                <button type="submit" class="btn btn-success btn-lg btn-block">Inscrire un utilisateur</button>
+                {!! Form::close() !!}
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <table class="table table-bordered">
+                        <thead class="thead-inverse">
+                            <tr>
+                                <th style="width: 10px">#</th>
+                                <th>Noms d'utilisateur</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tab_users as $unUser)
+                            <tr>
+                                <td class="col-md-1">
+                                    {{ $unUser["id"] }}
+                                </td>
+                                <td class="col-md-10" id="td{{ $unUser["id"] }}">
+                                    {{ $unUser["name"] }}
+                                </td>
+                                <td class="col-md-1">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            {!! Form::open(['route' => ["user.edit", $unUser->id], 'method' => 'get']) !!}
+                                            <button type="submit" class="btn btn-primary btn-circle"><i class="fa fa-list"></i></button>
+                                            {!! Form::close() !!}
+                                        </div>
+                                        <div class="col-md-6">
+                                            {!! Form::open(['route' => ["user.destroy", $unUser->id], 'method' => 'delete', 'id' => "form".$unUser->id]) !!}
+                                            <button type="submit" id="{{ $unArticle->id }}" class="btn btn-danger btn-circle jsDeleteButton"><i class="fa fa-times"></i></button>
+                                            {!! Form::close() !!}
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody> 
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
+
