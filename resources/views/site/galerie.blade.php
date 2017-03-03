0e046816d4de1b1993c6bd664c2a36f99ec95beb
nicolas BOUDET <nb@HP152.BESANCON.ELINTER>
2017-03-03 10:19:23 +0100

    fix: le fait que si un album sans image est actif fesait planter la page galerie

--- resources/views/site/galerie.blade.php
+++ resources/views/site/galerie.blade.php
@@ -7,12 +7,15 @@
         <div class="row contenu">
         @foreach ($lesAlbums as $unAlbum)
         @if($unAlbum->actif == true)
-        <div class="col-md-3">
+        <div class="col-md-3">@if(count($unAlbum->Photos) === 0)
+                    <center>Oups nous n'avons pas de photos a vous presentez dans cette album</center>
+                 @else
             <div class="grow pic ">
-                <a title="Image {{ $unAlbum["id"] }}" href="{{route('showGalerie',$unAlbum->id)}}">
-                    <img src="{{ url('img/galerie/miniature') ."/". $unAlbum->Photos->first()->fichier }}" alt="img{{ $unAlbum["id"] }}">                    
-                </a>
+                    <a title="album {{ $unAlbum["id"] }}" href="{{route('showGalerie',$unAlbum->id)}}">
+                        <img src="{{ url('img/galerie/miniature') ."/". $unAlbum->Photos->first()->fichier }}" alt="img{{ $unAlbum["id"] }}">                    
+                    </a>
             </div>
+                 @endif
             <center class="pirate"> {{$unAlbum["titre"]}}</center>
         </div> 
         @endif    
