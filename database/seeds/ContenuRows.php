cdae4171e23f26409f59dd533dd62e9facf2ad45
iLaziKx <iLaziKx@192.168.1.176>
2017-05-31 18:52:40 +0200

    Correction fichier seeding

--- database/seeds/ContenuRows.php
+++ database/seeds/ContenuRows.php
@@ -10,19 +10,13 @@ class ContenuRows extends Seeder {
      * @return void
      */
     public function run() {
-        DB::table('contenu')->insert([
-        ['page' => 'Le_club',
-        'titre' => 'Le club'],
-        ['page' => 'La_competition',
-        'titre' => 'La comptition'],
-        ['page' => 'Infos_Adherents',
-        'titre' => 'Infos Adhérents'],
-        ['page' => 'L_equipe_pedagogique',
-        'titre' => "L'équipe pédagogique"],
-        ['page' => 'L_ecole_de_tennis',
-        'titre' => "L'école de tennis"],
-        ['page' => 'Les_cours_collectifs_adultes',
-        'titre' => 'Les cours collectifs adultes']
+        DB::table('contenus')->insert([
+        ['page' => 'Le_club','ckeditor' => 'Texte','titre' => 'Le club'],
+        ['page' => 'La_competition','ckeditor' => 'Texte','titre' => 'La comptition'],
+        ['page' => 'Infos_Adherents','ckeditor' => 'Texte','titre' => 'Infos Adhérents'],
+        ['page' => 'L_equipe_pedagogique','ckeditor' => 'Texte','titre' => "L'équipe pédagogique"],
+        ['page' => 'L_ecole_de_tennis','ckeditor' => 'Texte','titre' => "L'école de tennis"],
+        ['page' => 'Les_cours_collectifs_adultes','ckeditor' => 'Texte','titre' => 'Les cours collectifs adultes']
         ]);
 
     }
